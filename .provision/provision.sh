6d04d27fb83158a156cb025da4cdeaade1a724e0
Geoffrey Challen <challen@buffalo.edu>
2016-01-08 15:26:02 -0500

    Move to minimal trusty box.

--- .provision/provision.sh
+++ .provision/provision.sh
@@ -1,13 +1,17 @@
 #!/bin/sh -e
 
-# 24 Dec 2015 : GWA : Install OS/161 toolchain and Git.
+# 24 Dec 2015 : GWA : Minimal box is very minimal. Use to get
+# add-apt-repository, updatedb, tmux.
+sudo apt-get install -y software-properties-common locate tmux
 add-apt-repository ppa:geoffrey-challen/os161-toolchain
 apt-get update 
+
+# 24 Dec 2015 : GWA : Install OS/161 toolchain and Git.
 apt-get install -y os161-toolchain git git-doc
 apt-get autoremove -y
 
 # 24 Dec 2015 : GWA : Bootstrap trinity user.
-useradd trinity -u 10000 -m -s /bin/bash
+useradd trinity -u 10000 -m -s /bin/bash 2>/dev/null
 rsync -a /etc/skel/ /home/trinity/
 
 mkdir /home/trinity/.ssh
@@ -29,7 +33,7 @@ echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
 service ssh reload
 
 echo "America/New_York" > /etc/timezone
-dpkg-reconfigure --frontend noninteractive tzdata
+dpkg-reconfigure --frontend noninteractive tzdata 2>/dev/null
 
 # 24 Dec 2015 : GWA : Remount shared folders with correct ownership on
 # every boot.
--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@ VAGRANT_COMMAND = ARGV[0]
 
 Vagrant.configure(2) do |config|
 
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "minimal/trusty64"
   config.vm.hostname = "zion"
 
   config.vm.synced_folder ".", "/vagrant", disabled: true
