5ffcc9c27f0b1d53b6815d3a9f7f7403c63a276a
Geoffrey Challen <challen@buffalo.edu>
2016-01-11 14:15:44 -0500

    Update provision script to be able to be run multiple times.

--- .provision/provision.sh
+++ .provision/provision.sh
@@ -2,8 +2,8 @@
 
 # 24 Dec 2015 : GWA : Minimal box is very minimal. Use to get
 # add-apt-repository, updatedb, tmux.
-sudo apt-get install -y software-properties-common locate tmux bash-completion
-add-apt-repository ppa:geoffrey-challen/os161-toolchain
+sudo apt-get install -y software-properties-common locate tmux bash-completion 
+add-apt-repository ppa:geoffrey-challen/os161-toolchain > /dev/null 2>&1 && true
 apt-get update 
 
 # 24 Dec 2015 : GWA : Install OS/161 toolchain and Git.
@@ -11,29 +11,32 @@ apt-get install -y os161-toolchain git git-doc
 apt-get autoremove -y
 
 # 24 Dec 2015 : GWA : Bootstrap trinity user.
-useradd trinity -u 10000 -m -s /bin/bash 2>/dev/null
-rsync -a /etc/skel/ /home/trinity/
+id -u trinity > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+	useradd trinity -u 10000 -m -s /bin/bash &>/dev/null
+	rsync -a /etc/skel/ /home/trinity/
 
-mkdir /home/trinity/.ssh
-cp /home/vagrant/.ssh/authorized_keys /home/trinity/.ssh/
-chmod 0700 /home/trinity/.ssh
+	mkdir /home/trinity/.ssh
+	cp /home/vagrant/.ssh/authorized_keys /home/trinity/.ssh/
+	chmod 0700 /home/trinity/.ssh
 
-echo "trinity ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/trinity
+	echo "trinity ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/trinity
 
-cp /home/vagrant/.bashrc /home/trinity/
-touch /home/trinity/.hushlogin
-mkdir -p /home/trinity/root
-cp /usr/share/examples/sys161/sys161.conf.sample /home/trinity/root/sys161.conf
-chown trinity:trinity -R /home/trinity/
+	cp /home/vagrant/.bashrc /home/trinity/
+	touch /home/trinity/.hushlogin
+	mkdir -p /home/trinity/root
+	cp /usr/share/examples/sys161/sys161.conf.sample /home/trinity/root/sys161.conf
+	chown trinity:trinity -R /home/trinity/
 
-# 24 Dec 2015 : GWA : Try to speed up SSH. Doesn't help much.
-echo >> /etc/ssh/sshd_config
-echo "UseDNS no" >> /etc/ssh/sshd_config
-echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
-service ssh reload
+	# 24 Dec 2015 : GWA : Try to speed up SSH. Doesn't help much.
+	echo >> /etc/ssh/sshd_config
+	echo "UseDNS no" >> /etc/ssh/sshd_config
+	echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
+	service ssh reload
 
-echo "America/New_York" > /etc/timezone
-dpkg-reconfigure --frontend noninteractive tzdata 2>/dev/null
+	echo "America/New_York" > /etc/timezone
+	dpkg-reconfigure --frontend noninteractive tzdata 2>/dev/null
+fi
 
 # 24 Dec 2015 : GWA : Remount shared folders with correct ownership on
 # every boot.
